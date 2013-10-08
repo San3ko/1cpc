194cf2f97b375c7820d1ac838cc00e790171b3cf
Ivan <ivantyurin@gmail.com>
2013-10-08 19:09:22 +0400

    pre-test

--- changer/v2/changer.pas
+++ changer/v2/changer.pas
@@ -1,23 +1,28 @@
-Program changer;
-uses crt;
-var	f:file of char;
-	l:text;
-	dir,lpath,endpath,pid,str2: string;
-	i,n: byte;
-Procedure getdir;
-	begin
-		if ParamStr(1) = '-dir' then
-      		dir:=paramstr(2);
-	end;
-Procedure change;
-	begin
-		endpath:=(dir+'\end.ini');
-		assign (f, endpath);
-			rewrite (f);
-			write (f, '1');
-		close (f);
-	end;
+program changer;
+uses
+  crt;
+var
+  f: file of char;
+  l: text;
+  dir, lpath, endpath, pid, str2: string;
+  i, n: byte;
+
+procedure getdir;
 begin
-	getdir;
-	change;
+  dir := paramstr(2);
+  pid := paramstr(4);
+end;
+
+procedure change;
+begin
+  endpath := (dir + '\' + pid + '\end.ini');
+  assign(f, endpath);
+  rewrite(f);
+  write(f, '1');
+  close(f);
+end;
+
+begin
+  getdir;
+  change;
 end.
